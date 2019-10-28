3da2f8a4ec25a1a47de1bf000f6333760c2e5eb2
K_ar_el <verhulstkarel@hotmail.be>
2019-10-28 14:43:32 +0100

    Create GhostAIBehaviour.cs

---
+++ Karel_Verhulst_NewMediaProject/Assets/Scripts/Character/Ghost AI/GhostAIBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostAIBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _destPoint;
+
+    private NavMeshAgent _ghost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ghost = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_ghost.destination = _destPoint.position;
+        _ghost.destination = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+    }
+    
+}
