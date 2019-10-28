92a306eea89181989f432c7397260fbf779c90f5
K_ar_el <verhulstkarel@hotmail.be>
2019-10-28 14:42:34 +0100

    refactor camera scipts

---
+++ Karel_Verhulst_NewMediaProject/Assets/Scripts/Camera/BaseCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCamera : MonoBehaviour
+{
+    public Transform _target;
+    public float _height = 10f;
+    public float _distance = 20f;
+    
+    void Start()
+    {
+        
+    }
+
+    void  Update()
+    {
+        HandleCamera();
+    }
+
+    protected virtual void HandleCamera()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (!_target)
+        {
+            return;
+        }
+    }
+   
+}
---
+++ Karel_Verhulst_NewMediaProject/Assets/Scripts/Camera/TopDownCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : BaseCamera
+{
+    [SerializeField]
+    private float _smoothSpeed = 0.5f;
+    [SerializeField]
+    private float _zoomSpeed = 1f;
+    [SerializeField]
+    private Vector2 _minMaxHeightClamp = Vector2.zero;
+    [SerializeField]
+    private Vector2 _minMaxDistanceClamp = Vector2.zero;
+    
+    protected override void HandleCamera()
+    {
+        base.HandleCamera();
+
+        ZoomBehaviour();
+
+        Vector3 worldposition = (Vector3.forward * -_distance) + (Vector3.up * _height);
+
+        Vector3 flatTargetPosition = _target.position;
+
+        flatTargetPosition.y = 0f;
+
+        Vector3 desiredPosition = _target.position + worldposition;
+
+        this.transform.position = Vector3.Slerp(transform.position, desiredPosition, Time.deltaTime * _smoothSpeed);
+       // this.transform.LookAt(_target);
+    }
+
+    private void ZoomBehaviour()
+    {
+        float vMovement = Mathf.RoundToInt(InputController.GetRightJoystick().z);
+
+        _distance += vMovement  * _zoomSpeed * Time.deltaTime;
+        _height += vMovement * _zoomSpeed * Time.deltaTime;
+      
+        _distance = Mathf.Clamp(_distance, _minMaxDistanceClamp.x, _minMaxDistanceClamp.y);
+        _height = Mathf.Clamp(_height, _minMaxHeightClamp.x, _minMaxHeightClamp.y);
+        
+    }
+}
