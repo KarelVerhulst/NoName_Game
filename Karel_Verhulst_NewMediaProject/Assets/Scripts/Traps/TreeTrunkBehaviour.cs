30071ed89c7a0b2ab8231697e4c12b74562902da
K_ar_el <verhulstkarel@hotmail.be>
2019-11-12 21:47:02 +0100

    Moving Tree Trunk
    Add script so a cube is scaling in the x axis and use it for a trap

---
+++ Karel_Verhulst_NewMediaProject/Assets/Scripts/Traps/TreeTrunkBehaviour.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTrunkBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _minScale;
+    [SerializeField]
+    private Vector3 _maxScale;
+    [SerializeField]
+    private bool _repeatable;
+    [SerializeField]
+    private float _speed = 2f;
+    [SerializeField]
+    private float _duration = 5f;
+    [SerializeField]
+    private float _startingWait = 0;
+    [SerializeField]
+    private float _waitAfterStartingScaling = 0;
+    [SerializeField]
+    private float _waitAfterEndingScaling = 0;
+
+    private bool _isAtTheEnd = false;
+    
+    IEnumerator Start()
+    {
+        
+        _minScale = this.transform.localScale;
+
+        yield return new WaitForSeconds(_startingWait);
+
+        while (_repeatable)
+        {
+            yield return ScaleOverTime(_minScale, _maxScale, _duration);
+        }
+    }
+
+
+
+    private IEnumerator ScaleOverTime(Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * _speed;
+
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+
+            if (this.transform.localScale == b)
+            {
+                yield return new WaitForSeconds(_waitAfterEndingScaling);
+                _isAtTheEnd = true;
+            }
+            
+
+            if (_isAtTheEnd)
+            {
+                
+                this.transform.localScale = Vector3.Lerp(b, a, i);
+            }
+            else
+            {
+                this.transform.localScale = Vector3.Lerp(a, b, i);
+            }
+
+            if (this.transform.localScale == a)
+            {
+                yield return new WaitForSeconds(_waitAfterStartingScaling);
+                _isAtTheEnd = false;
+            }
+
+
+            yield return null;
+        }
+
+    }
+}
